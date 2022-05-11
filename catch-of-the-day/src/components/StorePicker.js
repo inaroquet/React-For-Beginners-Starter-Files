8a729f9bfd228b75aadd0b5ebecf67854209e24a
inaroquet <88048322+inaroquet@users.noreply.github.com>
2022-05-11 14:55:42 -0500

    RFB-11 Using property set to arrow function to access 'this'

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,18 +2,9 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  // handleClick() {
-  //   alert("You clicked me!");
-  // }
-
-  constructor() {
-    super();
-    this.goToStore = this.goToStore.bind(this);
-  }
-
   myInput = React.createRef();
 
-  goToStore(event) {
+  goToStore = event => {
     event.preventDefault();
     // 1. stop the form from submitting (& refreshing page)
     console.log(this.myInput.current.value);
@@ -21,15 +12,13 @@ class StorePicker extends React.Component {
 
     // 3. change page to /store/value-from-input
 
-  }
+  };
 
   render() {
     return (
       <>
-        {/* This is a comment in javascript */}
         <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
-          {/* <button className="button" onClick={this.handleClick()}>Click Me!</button> */}
           <input
             type="text"
             ref={this.myInput}
